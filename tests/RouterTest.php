a9e45d8dc89e1fe6251e32f4e83cdc02ee739957
Jack Wan <hwguyguy@gmail.com>
2018-06-11 11:20:08 +0800

    Update tests

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -37,10 +37,11 @@ EOD;
 /**
  * @var \\Rise\\Router\\Scope \$scope
  */
+\$scope->get('', 'Home.index', 'root');
+\$scope->get('contact', 'Contact.index', 'contact');
 \$scope->createScope(function(\$scope) {
-	\$scope->get('', 'Home.index', 'root');
-	\$scope->get('contact', 'Contact.index', 'contact');
-	\$scope->get('products/{id}', 'Product.show', 'productDetail');
+	\$scope->setPrefix('products');
+	\$scope->get('{id}', 'Product.show', 'productDetail');
 });
 EOD;
 
